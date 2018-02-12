b5fc4fa54635a9dd83e34b655f4e294cf8e257a3
mkucharz <m@kucharz.net>
2018-02-12 17:52:08 +0100

    wip

---
+++ test/resize.test.js
@@ -0,0 +1,17 @@
+/* global describe it */
+// import fs from 'fs'
+// import path from 'path'
+// import {assert} from 'chai'
+// import {run} from '@syncano/test'
+
+describe('resize', function () {
+  it('resize', async () => {
+    // const res = await run('resize', {args: {}})
+
+    // console.log(res)
+    // assert.propertyVal(res, 'code', 200)
+    // assert.propertyVal(res, 'mimetype', 'application/pdf')
+
+    // fs.writeFileSync(path.join(__dirname, '.results/pdf1.pdf'), res.data)
+  })
+})
